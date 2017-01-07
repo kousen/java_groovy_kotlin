36a51e50a2f5e81d2ff810ee77abf5ec22d9973a
Kenneth Kousen <ken.kousen@kousenit.com>
2017-01-07 16:51:00 +0530

    Add MapFilterReduce test and impl

--- src/main/java/streams/MapFilterReduce.java
+++ src/main/java/streams/MapFilterReduce.java
@@ -2,19 +2,39 @@ package streams;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.IntPredicate;
 
 public class MapFilterReduce {
-    public static void main(String[] args) {
-        List<String> strings = Arrays.asList("this", "is", "a",
-                "list", "of", "strings");
+    public static final IntPredicate EVENS = n -> n % 2 == 0;
+    public static final IntPredicate ODDS = n -> n % 2 != 0;
 
-        List<Integer> ints = strings.stream()
+    private List<String> strings = Arrays.asList("this", "is", "a",
+            "list", "of", "strings");
+
+    public int sumAllLengths() {
+        return strings.stream()
+                .mapToInt(String::length)
+                .sum();
+    }
+
+    public int sumEvenLengthStrings() {
+        return strings.stream()
                 .mapToInt(String::length)
                 .filter(n -> n % 2 == 0)
-                .boxed()
-                .collect(Collectors.toList());
+                .sum();
+    }
 
+    public int sumOddLengthStrings() {
+        return strings.stream()
+                .mapToInt(String::length)
+                .filter(n -> n % 2 != 0)
+                .sum();
+    }
 
+    public int sumStrings(IntPredicate predicate) {
+        return strings.stream()
+                .mapToInt(String::length)
+                .filter(predicate)
+                .sum();
     }
 }
---
+++ src/test/groovy/streams/MapFilterReduceSpec.groovy
@@ -0,0 +1,34 @@
+package streams
+
+import spock.lang.Specification
+
+class MapFilterReduceSpec extends Specification {
+    MapFilterReduce demo = new MapFilterReduce()
+
+    def "SumAllLengths"() {
+        expect:
+        20 == demo.sumAllLengths()
+    }
+
+    def "SumEvenLengthStrings"() {
+        expect:
+        12 == demo.sumEvenLengthStrings()
+    }
+
+    def "SumOddLengthStrings"() {
+        expect:
+        8 == demo.sumOddLengthStrings()
+    }
+
+    boolean isEven(int n) {
+        n % 2 == 0
+    }
+
+    def "SumStringsWithPredicate"() {
+        expect:
+        12 == demo.sumStrings(this.&isEven)
+        8 == demo.sumStrings { it % 2 != 0 }
+        12 == demo.sumStrings(MapFilterReduce.EVENS)
+        8 == demo.sumStrings(MapFilterReduce.ODDS)
+    }
+}
