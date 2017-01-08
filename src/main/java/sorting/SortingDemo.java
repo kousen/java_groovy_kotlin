71c538f70ec1297f87bd24246847ad60375d95ef
Kenneth Kousen <ken.kousen@kousenit.com>
2017-01-08 07:59:53 +0530

    Revised starter code for string sorts

--- src/main/java/sorting/SortingDemo.java
+++ src/main/java/sorting/SortingDemo.java
@@ -5,24 +5,18 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static java.util.Comparator.*;
-import static java.util.stream.Collectors.toList;
-
 public class SortingDemo {
     private List<String> sampleStrings =
             Arrays.asList("this", "is", "a", "list", "of", "strings");
 
     // Default sort from Java 7-
     public List<String> alphaSort() {
-        Collections.sort(sampleStrings);
         return sampleStrings;
     }
 
     // Default sort from Java 8+
     public List<String> alphaSortUsingStreams() {
-        return sampleStrings.stream()
-                .sorted()
-                .collect(toList());
+        return sampleStrings;
     }
 
     // Java 7- using Comparator with anonymous inner class
@@ -38,38 +32,26 @@ public class SortingDemo {
 
     // Using a lambda as a Comparator with a lambda
     public List<String> lengthSortWithLambda() {
-        Collections.sort(sampleStrings,
-                (s1, s2) -> s1.length() - s2.length());
         return sampleStrings;
     }
 
     // Sort by length with sorted
     public List<String> lengthSortUsingSorted() {
-        return sampleStrings.stream()
-                .sorted((s1, s2) -> s1.length() - s2.length())
-                .collect(toList());
+        return sampleStrings;
     }
 
     // Length sort with comparingInt
     public List<String> lengthSortUsingComparator() {
-        return sampleStrings.stream()
-                .sorted(Comparator.comparingInt(String::length))
-                .collect(toList());
+        return sampleStrings;
     }
 
     // Sort by length then alpha using sorted
     public List<String> lengthSortThenAlphaSort() {
-        return sampleStrings.stream()
-                .sorted(comparing(String::length)
-                        .thenComparing(naturalOrder()))
-                .collect(toList());
+        return sampleStrings;
     }
 
     // Sort by length then reverse alpha using sorted
     public List<String> lengthSortThenReverseAlphaSort() {
-        return sampleStrings.stream()
-                .sorted(comparing(String::length)
-                        .thenComparing(reverseOrder()))
-                .collect(toList());
+        return sampleStrings;
     }
 }
