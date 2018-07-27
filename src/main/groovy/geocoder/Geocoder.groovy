18ba4f4f90b24ea54d147442e9e4e53674bddefc
Ken Kousen <ken.kousen@kousenit.com>
2018-07-27 00:18:13 -0500

    Update geocoder to use key at json endpoint

--- src/main/groovy/geocoder/Geocoder.groovy
+++ src/main/groovy/geocoder/Geocoder.groovy
@@ -1,17 +1,21 @@
 package geocoder
 
+import groovy.json.JsonSlurper
+
 class Geocoder {
     public static final String BASE =
-            'https://maps.googleapis.com/maps/api/geocode/xml?'
+            'https://maps.googleapis.com/maps/api/geocode/json?'
+    private static final String KEY =
+            'AIzaSyDw_d6dfxDEI7MAvqfGXEIsEMwjC1PWRno'
 
     void fillInLatLng(Location location) {
         String encoded = [location.street, location.city,
                           location.state].collect {
             URLEncoder.encode(it, 'UTF-8')
         }.join(',')
-        String qs = "address=$encoded"
-        def root = new XmlSlurper().parse("$BASE$qs")
-        def loc = root.result[0].geometry.location
+        String qs = "address=$encoded&key=$KEY"
+        def root = new JsonSlurper().parse("$BASE$qs")
+        def loc = root.results[0].geometry.location
         location.latitude = loc.lat.toBigDecimal()
         location.longitude = loc.lng.toBigDecimal()
     }
